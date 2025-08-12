4eb56b024fee909f5496e068ddf37d0d93be1918
ThisMayBePacman <studepyton@outlook.com>
2025-08-12 02:31:50 +0200

    cleaning config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ TIMEFRAMES = { 'M15': '15m', 'M5': '5m' }
 LOOKBACK = 100
 TICK_SIZE = 0.5
 POLL_INTERVAL = 10  # en secondes
-DB_PATH = "donnees/trading.db"
+
 
 # Sélection de stratégie au runtime
 STRATEGY: str | None = None   # None ou "trailing_sl_only" | "trailing_sl_and_tp"
